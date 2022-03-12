1353a2611c2f3eab0163d4363d33acc93b77dcd7
mAnu4ka <94441221+mAnu4ka@users.noreply.github.com>
2022-03-12 10:58:37 +0500

    добавил коменты

--- js/script.js
+++ js/script.js
@@ -23,9 +23,10 @@ const movieDB = {
         "Ла-ла лэнд",
     ]
 };
+//тут сортировка верхнего масива
 movieDB.movies.sort()
 
-
+// нахождение нужного элемента(скипаем этот момент)
 let promo__adv = document.querySelector('.promo__adv')
 let img__chek = document.querySelector('img')
 let promo__genre = document.querySelector('.promo__genre')
@@ -34,19 +35,31 @@ let imgas = promo__adv.children
 let sda = promo__adv.children[1].typeof
 let count = 0
 let vseParray = Array.from(imgas)
+//вот все img 
+let img = document.querySelectorAll('img[alt="some picture"]')
 let result = vseParray.filter(word => word.length == img__chek.length);
 let promo__bg = document.querySelector('.promo__bg');
 let ul = document.querySelector('.promo__interactive-list')
 let form = document.querySelector('.add')
+//с инета взял но понял!!
 const changeBgImg = () => {
     promo__bg.style.backgroundImage = `url(./bg.jpg)`;
     promo__genre.innerText = 'драма'
 }
-
+// сделал п оп ростому удаление рекламы
+const Remouvrm = () =>{
+    for (const item of img) {
+        item.remove()
+    }
+}
+// самое простое создание элемента
 const CreateElement = () => {
+    //очистка элементов чтобы не было багов
     ul.innerHTML = " "
+    //заново преопределяю каунт чтобы не было багов
     count = 0
     for (const item of movieDB.movies) {
+        //прибовление
         count++
         let li = document.createElement('li')
         let div = document.createElement('div')
@@ -55,8 +68,9 @@ const CreateElement = () => {
         li.classList.add('promo__interactive-item')
         li.append(div)
         ul.append(li)
-        console.log(movieDB.movies);
     }
 }
+// ну вот всё поставте звёздочку
 changeBgImg();
 CreateElement()
+Remouvrm()
